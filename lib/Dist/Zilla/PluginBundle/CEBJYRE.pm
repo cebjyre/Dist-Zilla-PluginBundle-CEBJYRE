76bf8f89ac8e79eb945d4ab06df83fee13d6236b
Glenn Fowler <cebjyre@cpan.org>
2011-02-11 20:32:33 +1100

    Bundle created
    First version of the bundle.

--- lib/Dist/Zilla/PluginBundle/CEBJYRE.pm
+++ lib/Dist/Zilla/PluginBundle/CEBJYRE.pm
@@ -2,4 +2,51 @@ use strict;
 use warnings;
 package Dist::Zilla::PluginBundle::CEBJYRE;
 
+# ABSTRACT: My default dzil plugins
+
+use Moose;
+with 'Dist::Zilla::Role::PluginBundle::Easy';
+
+=head1 DESCRIPTION
+
+Using this bundle is equivalent to:
+
+  [PkgVersion]
+  [AutoPrereqs]
+  [PodWeaver]
+  [PodCoverageTests]
+  [PodSyntaxTests]
+  [NextRelease]
+  [CheckPrereqsIndexed]
+  [@Basic]
+  [@Git]
+
+=method configure
+
+See L<Dist::Zilla::Role::PluginBundle::Easy>.
+
+=cut
+
+sub configure {
+  my $self = shift;
+
+  $self->add_plugins(qw(
+    PkgVersion
+    AutoPrereqs
+    PodWeaver
+    PodCoverageTests
+    PodSyntaxTests
+    NextRelease
+    CheckPrereqsIndexed
+  ));
+
+  $self->add_bundle(
+    'Basic',
+    'Git',
+  );
+}
+
+__PACKAGE__->meta->make_immutable;
+no Moose;
+
 1;
